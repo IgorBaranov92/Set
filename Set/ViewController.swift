ebd35c942e4c8be52feabdc2f89aaf4b58dbd63e
Igor <pokerman154@gmail.com>
2019-10-31 09:36:39 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Igor on 31.10.2019.
+//  Copyright © 2019 VL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
